0b72fc48aa199f26528f00b2bbde4e654d437d80
Jean Candido <146030723+JeanCandido@users.noreply.github.com>
2024-08-21 13:39:40 -0300

    a

---
+++ 1-hello-world/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className=" w-screen h-screen flex aling-center justify-center">
+      <h1 className="text-4xl flex aling-center justify-center">Hello Word</h1>
+    </main>
+  );
+}
