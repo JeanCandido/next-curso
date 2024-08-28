27b35b6d7d77614a8471ff2f2cfdaa8d6e8d16c8
Jean Candido <146030723+JeanCandido@users.noreply.github.com>
2024-08-28 15:06:44 -0300

    Update layout.js

--- 1-hello-world/src/app/layout.js
+++ 1-hello-world/src/app/layout.js
@@ -18,7 +18,13 @@ export default function RootLayout({ children }) {
       </head>
       <body className={inter.className}>
         <NavBar/>
-        
+        <ul className="bg-black text-white flex gap-6 p-2">
+          <Link href="/" className=" hover:bg-slate-800 p-2 rounded">Home</Link>
+          <br/>
+          <Link href="/products" className=" hover:bg-slate-800 p-2 rounded">Produtos</Link>
+          <br/>
+          <Link href="/about" className=" hover:bg-slate-800 p-2 rounded">Sobre nós</Link>
+        </ul>
         {children}
       </body>
     </html>
